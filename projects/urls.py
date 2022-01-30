9c7df4ba99711d0120d343ca398e182da7ede815
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-01-31 01:43:15 +0600

    list dict added

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('about/', views.about, name = 'about us'),
     path('', views.project, name = 'project'),
-    path('single-project/', views.single_project, name = 'projects'),
+    path('single/<str:pk>/', views.singles, name = 'single'),
    
    ]
--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,46 @@
+from pydoc import describe
 from django.shortcuts import render
 
 # Create your views here.
 from unicodedata import name
 from django.shortcuts import HttpResponse
+
+
 def about(request):
     return render(request, 'about.html')
 
+
+list_dict = [
+    {
+        'id': 1,
+        'title': 'E-commerce project',
+        'description': 'This is a full featured e-commerce website'
+    },
+    {
+        'id' : 2,
+        'title' : 'Blog Web',
+        'description': 'This is public blogsite'
+    },
+
+    {
+        'id': 3,
+        'title': 'Python web',
+        'description': 'This website for learning python'
+    },
+
+]
+
+
 def project(request):
-    return render(request, 'project/projects.html')
+    msg = "You are in the project page"
+    number = 10
+    context = {'message': msg, 'Number': number, 'list_dict':list_dict}
+    return render(request, 'project/projects.html', context)
 
 
-
-
-def single_project(request):
-     return render(request, 'project/single-project.html')
+def singles(request, pk):
+    projectobj = None
+    for i in list_dict:
+        if i['id'] == pk:
+            projectobj = i
+    return render(request, 'project/single-project.html', {'singles': projectobj},)
